d0451c0c6bea90a7c02015ab0399778a92736f4a
Alex Lowther <85571348+alexander-lowther@users.noreply.github.com>
2021-11-24 15:44:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  //here your code
 
-document.getElementById("v1").addEventListener("mouseover", function() {
-	this.play();
-});
+	document.getElementById("v1").addEventListener("mouseover", function() {
+		this.play();
+	});
 
-document.getElementById("v1").addEventListener("mouseleave", function() {
-	this.pause();
-});
+	document.getElementById("v1").addEventListener("mouseleave", function() {
+		this.pause();
+	});
 
-document.getElementById("v2").addEventListener("mouseover", function() {
-	this.play();
-});
+	document.getElementById("v2").addEventListener("mouseover", function() {
+		this.play();
+	});
 
-document.getElementById("v2").addEventListener("mouseleave", function() {
-	this.pause();
-});
+	document.getElementById("v2").addEventListener("mouseleave", function() {
+		this.pause();
+	});
 
-document.getElementById("v3").addEventListener("mouseover", function() {
-	this.play();
-});
+	document.getElementById("v3").addEventListener("mouseover", function() {
+		this.play();
+	});
 
-document.getElementById("v3").addEventListener("mouseleave", function() {
-	this.pause();
-});
+	document.getElementById("v3").addEventListener("mouseleave", function() {
+		this.pause();
+	});
 
-document.getElementById("v4").addEventListener("mouseover", function() {
-	this.play();
-});
+	document.getElementById("v4").addEventListener("mouseover", function() {
+		this.play();
+	});
 
-document.getElementById("v4").addEventListener("mouseleave", function() {
-	this.pause();
+	document.getElementById("v4").addEventListener("mouseleave", function() {
+		this.pause();
+	});
+	
 });
